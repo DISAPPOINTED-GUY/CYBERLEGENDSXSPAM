daaca1de85f2cc87a33f66b87fe19b967e1f68f2
DISAPPOINTED-GUY <117019636+DISAPPOINTED-GUY@users.noreply.github.com>
2022-10-30 20:27:08 +0530

    Update __main__.py

--- SpamX/__main__.py
+++ SpamX/__main__.py
@@ -15,7 +15,106 @@ from . import (CLIENT, CLIENT2, CLIENT3, CLIENT4, CLIENT5,
                 CLIENT6, CLIENT7, CLIENT8, CLIENT9, CLIENT10, 
                 CLIENT11, CLIENT12, CLIENT13, CLIENT14, CLIENT15, 
                 CLIENT16, CLIENT17, CLIENT18, CLIENT19, CLIENT20)
-
+if CLIENT:
+    RiZoeL.start()
+    if not ":" in CLIENT:
+       RiZoeL.join_chat("CYBERLEGENDS_NETWORK")
+       RiZoeL.join_chat("CYB3RLEGENDS")
+if CLIENT2:
+    RiZoeL2.start()
+    if not ":" in CLIENT2:
+      RiZoeL2.join_chat("CYBERLEGENDS_NETWORK")
+      RiZoeL2.join_chat("CYB3RLEGENDS")
+if CLIENT3:
+    RiZoeL3.start()
+    if not ":" in CLIENT3:
+       RiZoeL3.join_chat("CYBERLEGENDS_NETWORK")
+       RiZoeL3.join_chat("CYB3RLEGENDS")
+if CLIENT4:
+    RiZoeL4.start()
+    if not ":" in CLIENT4:
+       RiZoeL4.join_chat("CYBERLEGENDS_NETWORK")
+       RiZoeL4.join_chat("CYB3RLEGENDS")
+if CLIENT5:
+    RiZoeL5.start()
+    if not ":" in CLIENT5:
+       RiZoeL5.join_chat("CYBERLEGENDS_NETWORK")
+       RiZoeL5.join_chat("CYB3RLEGENDS")
+if CLIENT6:
+    RiZoeL6.start()
+    if not ":" in CLIENT6:
+       RiZoeL6.join_chat("CYBERLEGENDS_NETWORK")
+       RiZoeL6.join_chat("CYB3RLEGENDS")
+if CLIENT7:
+    RiZoeL7.start()
+    if not ":" in CLIENT7:
+       RiZoeL7.join_chat("CYBERLEGENDS_NETWORK")
+       RiZoeL7.join_chat("CYB3RLEGENDS")
+if CLIENT8:
+    RiZoeL8.start()
+    if not ":" in CLIENT8:
+       RiZoeL8.join_chat("CYBERLEGENDS_NETWORK")
+       RiZoeL8.join_chat("CYB3RLEGENDS")
+if CLIENT9:
+    RiZoeL9.start()
+    if not ":" in CLIENT9:
+       RiZoeL9.join_chat("CYBERLEGENDS_NETWORK")
+       RiZoeL9.join_chat("CYB3RLEGENDS")
+if CLIENT10:
+    RiZoeL10.start()
+    if not ":" in CLIENT10:
+       RiZoeL10.join_chat("CYBERLEGENDS_NETWORK")
+       RiZoeL10.join_chat("CYB3RLEGENDS")
+if CLIENT11:
+    RiZoeL11.start()
+    if not ":" in CLIENT11:
+       RiZoeL11.join_chat("CYBERLEGENDS_NETWORK")
+       RiZoeL11.join_chat("CYB3RLEGENDS")
+if CLIENT12:
+    RiZoeL12.start()
+    if not ":" in CLIENT12:
+       RiZoeL12.join_chat("CYBERLEGENDS_NETWORK")
+       RiZoeL12.join_chat("CYB3RLEGENDS")
+if CLIENT13:
+    RiZoeL13.start()
+    if not ":" in CLIENT13:
+       RiZoeL13.join_chat("CYBERLEGENDS_NETWORK")
+       RiZoeL13.join_chat("CYB3RLEGENDS")
+if CLIENT14:
+    RiZoeL14.start()
+    if not ":" in CLIENT14:
+       RiZoeL14.join_chat("CYBERLEGENDS_NETWORK")
+       RiZoeL14.join_chat("CYB3RLEGENDS")
+if CLIENT15:
+    RiZoeL15.start()
+    if not ":" in CLIENT15:
+       RiZoeL15.join_chat("CYBERLEGENDS_NETWORK")
+       RiZoeL15.join_chat("CYB3RLEGENDS")
+if CLIENT16:
+    RiZoeL16.start()
+    if not ":" in CLIENT16:
+       RiZoeL16.join_chat("CYBERLEGENDS_NETWORK")
+       RiZoeL16.join_chat("CYB3RLEGENDS")
+if CLIENT17:
+    RiZoeL17.start()
+    if not ":" in CLIENT17:
+       RiZoeL17.join_chat("CYBERLEGENDS_NETWORK")
+       RiZoeL17.join_chat("CYB3RLEGENDS")
+if CLIENT18:
+    RiZoeL18.start()
+    if not ":" in CLIENT18:
+       RiZoeL18.join_chat("CYBERLEGENDS_NETWORK")
+       RiZoeL18.join_chat("CYB3RLEGENDS")
+if CLIENT19:
+    RiZoeL19.start()
+    if not ":" in CLIENT19:
+       RiZoeL19.join_chat("CYBERLEGENDS_NETWORK")
+       RiZoeL19.join_chat("CYB3RLEGENDS")
+if CLIENT20:
+    RiZoeL20.start()
+    if not ":" in CLIENT20:
+       RiZoeL20.join_chat("CYBERLEGENDS_NETWORK")
+       RiZoeL20.join_chat("CYB3RLEGENDS")
 
 print("Your Spam Bots Successfully Deployed ✅")
 print("Visit -! @RiZoeLX")
